a6e431f1bf2985cc51d4d50e9af95753a7929c3b
Chris Harrington <chrisharrington99@gmail.com>
2025-08-16 20:12:29 +0000

    Missing file.

---
+++ components/playButton/style.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        position: 'relative',
+    },
+
+    clock: {
+        position: 'absolute',
+        bottom: -4,
+        right: -4,
+    },
+});
